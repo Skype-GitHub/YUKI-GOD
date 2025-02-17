2abec00bda5326faa22ab1be3829f42334c9a2ef
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2025-02-17 16:49:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import random
 import os
 import subprocess
 from cache import cache
-ver = "2.7.9.1" # バージョン    
-update = "YUKIBBSのID認証に対応" # アップデート内容
+ver = "2.7.9.2" # バージョン    
+update = "" # アップデート内容
 token = "e4f5c13f-4f31-4ae1-ac5c-b3f1df232073" # hcaptchaのサイトキー
 max_api_wait_time = 5
 max_time = 10
